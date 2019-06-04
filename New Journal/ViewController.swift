fa00b2e021f367b8763bf043c1776d71355bc8e5
Alex <alexcc17@gmail.com>
2019-06-04 15:31:55 -0400

    Initial Commit

---
+++ New Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New Journal
+//
+//  Created by Alex on 6/4/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
